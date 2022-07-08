9fa55f1592d2084eccd3c1f9acc8612b47da240a
Murat Y <93838693+Stuckinrecoverymode@users.noreply.github.com>
2022-07-08 13:32:41 +0300

    multiple file convertion in ffmpeg

---
+++ mutliple_file_convertion.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+srcExt=$1
+destExt=$2
+
+srcDir=$3
+destDir=$4
+
+for filename in "$srcDir"/*.$srcExt; do
+
+        basePath=${filename%.*}
+        baseName=${basePath##*/}
+
+        ffmpeg -i "$filename" "$destDir""$baseName"."$destExt"
+done
+
+echo "Conversion from ${srcExt} to ${destExt} complete!"
